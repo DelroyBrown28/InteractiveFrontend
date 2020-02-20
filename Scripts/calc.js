90917b2c929fe192bc7b796b006a6c2bd0b98aa1
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-02-20 17:52:15 +0000

    Fixing our tests

--- Scripts/calc.js
+++ Scripts/calc.js
@@ -1,7 +1,10 @@
-function addition(numOne, numTwo) {
-    if(typeof(numOne) == "number" && typeof(numTwo) ==
-    "number") {
-        return numOne + numTwo;
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number) {
+    if (typeOf(number) == "number") {
+        this.value += number;
     } else {
         alert("Error!");
     }
